8814ddbb92ce29253881d84cf5eed9ee056df7ec
KaparZhekshen <kapar.zhekshen@architech.nyc>
2022-09-28 15:53:03 +0600

    test commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Text from "./Text";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+        <h2>Master</h2>
+        <Text/>
     </div>
   );
 }
---
+++ src/Text/index.js
@@ -0,0 +1,11 @@
+import '../App.css';
+
+function Text() {
+    return (
+        <div className="App">
+            <h1>Test</h1>
+        </div>
+    );
+}
+
+export default Text;
